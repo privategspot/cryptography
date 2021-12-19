35d7270f91111e27e28791a08fac6083b9d1c370
Daniil Frangov <frangov7@gmail.com>
2021-12-19 20:44:48 +0300

    Add permutation module

---
+++ permutation.py
@@ -0,0 +1,77 @@
+import logging
+
+from typing import List
+
+
+def _tokenize(message, token_size):
+    message_len = len(message)
+    return [message[i:i+token_size] for i in range(0, message_len, token_size)]
+
+
+def _permutate(tokens: List[str], key: List[int], key_len: int, reverse: bool = False):
+    result = []
+    for token in tokens:
+        permutation = list(token)
+        for i in range(key_len):
+            if not reverse:
+                permutation[int(key[i] - 1)] = token[i]
+                continue
+            permutation[i] = token[int(key[i] - 1)]
+        result = result + permutation
+    return result
+
+
+def encrypt(message: str, key: List[int]):
+    """
+    Шифрует сообщение методом блочной перестановки.
+
+    :param str message: Шифротекст
+    :param List[int] key: Ключ шифрования блочной перестановки
+
+    :return: Шифротекст message зашифрованный при помощи ключа key
+    """
+    logging.debug(
+        f'start block permutation method encryption with message "{message}" and key "{key}"'
+    )
+
+    key_len = len(key)
+    tokens = _tokenize(message, key_len)
+
+    # если полседний токен меньше, чем длина ключа, то дополняем пробелами
+    last_token = tokens[-1]
+    if len(last_token) < key_len:
+        tokens[-1] = last_token.ljust(key_len)
+
+    logging.debug(f"message tokens is {tokens}")
+
+    permutation = _permutate(tokens, key, key_len)
+
+    return "".join(permutation)
+
+
+def decrypt(message, key):
+    """
+    Дешфирует сообщение, зашифрованное методом блочной перестановки.
+
+    :param str message: Зашифрованный методом блочной перестановки шифротекст
+    :param List[int] key: Ключ шифрования блочной перестановки
+
+    :return: Шифротекст message расшифрованный при помощи ключа key
+    """
+    logging.debug(
+        f'start block permutation method decryption with message "{message}" and key "{key}"'
+    )
+
+    key_len = len(key)
+    tokens = _tokenize(message, key_len)
+
+    # если полседний токен меньше, чем длина ключа, то дополняем пробелами
+    last_token = tokens[-1]
+    if len(last_token) < key_len:
+        tokens[-1] = last_token.ljust(key_len)
+
+    logging.debug(f"message tokens is {tokens}")
+
+    permutation = _permutate(tokens, key, key_len, reverse=True)
+
+    return "".join(permutation).strip()
