15483c2abf61db499177bfbe3899e260b2138ec6
Daniil Frangov <frangov7@gmail.com>
2021-12-26 03:40:57 +0300

    Refactoring

--- util.py
+++
@@ -1,7 +0,0 @@
-def tokenize(message, token_size):
-    message_len = len(message)
-    return [message[i:i+token_size] for i in range(0, message_len, token_size)]
-
-
-def pad_with(char: str, subject: str, size: int):
-    return subject.ljust(size, char)
